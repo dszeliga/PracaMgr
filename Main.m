b151bbc89cb677dd8c87e431dcb58ec7a08f1fd0
dszeliga <szeliga_d@o2.pl>
2018-06-18 16:54:04 +0200

    any spectrum of sound

--- Main.m
+++ Main.m
@@ -1,20 +1,55 @@
+clear all;
+close all;
+clc;
 %Wczytanie danych
-path = '';
+path = 'C:\Users\LastWerewolf\Desktop\pracaMgr\nagrania podejscie 2\DS_11.wav';
 sound = ReadSound(path);
+info = audioinfo('C:\Users\LastWerewolf\Desktop\pracaMgr\nagrania podejscie 2\DS_11.wav');
 
+Fs=info.SampleRate;
+T=info.TotalSamples/Fs;
+t=0:1/Fs:T-(1/Fs);
+
+subplot(2,2,1);
+plot(t, sound);
+%plot(sound);
+title('Original Signal')
+%xlabel('number of samples')
+xlabel('Time[s]');
+ylabel('Amplitude')
+
+sound = sound(40000:50000);
+t1=0:1/Fs:((length(sound)/Fs)-(1/Fs));
+subplot(2,2,2);
+plot(t1,sound);
+title('Selected Signal');
+xlabel('Time [s]')
+% xlabel('number of samples');
+ylabel('Amplitude');
 %Segmentacja polozenia gloski w wyrazie
-selectedCharacter = SegmentationOfWord(sound);
+%selectedCharacter = SegmentationOfWord(sound);
 
 %Segmentacja faz w glosce (nastep, szczyt, zestep)
-selectedPhase = SegmentationOfCharacter(selectedCharacter);
+%selectedPhase = SegmentationOfCharacter(selectedCharacter);
 
 %Analiza widmowa (FFT?)
-soundAfterFFT = SpectralAnalysis(selectedPhase);
+[amplitudeSpectrum, phaseSpectrum] = SpectralAnalysis(sound);
 
+subplot(2,2,3);
+plot(amplitudeSpectrum);
+title('Amplitude Spectrum')
+xlabel('Frequenzy [Hz]');
+ylabel('Amplitude')
+
+subplot(2,2,4);
+plot(phaseSpectrum);
+title('Phase Spectrum')
+xlabel('Frequenzy [Hz]');
+ylabel('Phase')
 %Analiza widma gestosci mocy
-soundAfterPDS = PowerDensitySpectrumAnalysis(selectedPhase);
+%soundAfterPDS = PowerDensitySpectrumAnalysis(selectedPhase);
 
 % jakies tam dalsze przetwarzanie pewnie...
 
 %Klasyfikacja
-Classification(data);
+%Classification(data);
--- SpectralAnalysis.m
+++ SpectralAnalysis.m
@@ -1,6 +1,7 @@
-function [dataAfterFFT] = SpectralAnalysis(data)
+function [amplitudeSpectrum,phaseSpectrum] = SpectralAnalysis(sound)
 %Analiza widmowa (FFT?)
-
-
+signal = fft(sound); %obliczanie DFT ci¹gu x
+amplitudeSpectrum = abs(signal); % widmo amplitudowe
+phaseSpectrum = unwrap(angle(signal)); %widmo fazowe;
 end
 
